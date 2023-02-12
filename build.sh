f9f58021cc4e880fc37fb147c0988371a489b63a
Setareh Karimi <s.karimi@coinito.local>
2023-02-12 10:33:33 +0330

    enhanc: improve docker compose file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p 'if you want build one of services please enter the name, otherwise just skip it':  appname
-
-if test -z "$appname" 
-then
-      docker-compose build
-else
-      docker-compose build $appname
-fi
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p 'if you want run one of services please enter the name, otherwise just skip it':  appname
-
-if test -z "$appname" 
-then
-      docker-compose up -d
-else
-      docker-compose up -d $appname
-fi
